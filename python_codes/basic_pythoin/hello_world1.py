030e452c6a271ffbf86b9cc7bf5d94a46e9f43d6
bapuoldtown <bapucodetest1987@gmail.com>
2022-07-31 15:01:25 +0530

    Added the set as return values

--- python_codes/basic_pythoin/hello_world1.py
+++ python_codes/basic_pythoin/hello_world1.py
@@ -14,8 +14,13 @@ def dictionary_update():
     return {**{"guru": "oldtown"}, **{"krisha": "bangalore"}}
 
 
+def hashmap_update():
+    return {"guru", "bapu", "krisha"}
+
+
 if __name__ == '__main__':
     print(greet_english())
     print(greet_hindi())
     print(greet_german())
     print(dictionary_update())
+    print(hashmap_update())
