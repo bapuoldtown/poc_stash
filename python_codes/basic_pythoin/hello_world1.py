2818d2241f3eb4b4b799019bd64823c1398dda68
bapuoldtown <bapucodetest1987@gmail.com>
2022-07-31 14:59:19 +0530

    second commit

--- python_codes/basic_pythoin/hello_world1.py
+++ python_codes/basic_pythoin/hello_world1.py
@@ -10,8 +10,12 @@ def greet_german():
     return "Guten Morgen World!!"
 
 
+def dictionary_update():
+    return {**{"guru": "oldtown"}, **{"krisha": "bangalore"}}
+
+
 if __name__ == '__main__':
     print(greet_english())
     print(greet_hindi())
     print(greet_german())
-    
+    print(dictionary_update())
