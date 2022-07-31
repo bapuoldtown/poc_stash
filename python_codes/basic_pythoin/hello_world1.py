5568b25e4b2b38a7b392e13b7afd3a02fe1a33db
bapuoldtown <bapucodetest1987@gmail.com>
2022-07-31 14:55:00 +0530

    adding new file

---
+++ python_codes/basic_pythoin/hello_world1.py
@@ -0,0 +1,17 @@
+def greet_english():
+    return "Hello World!!!"
+
+
+def greet_hindi():
+    return "Namastey Bharat!!"
+
+
+def greet_german():
+    return "Guten Morgen World!!"
+
+
+if __name__ == '__main__':
+    print(greet_english())
+    print(greet_hindi())
+    print(greet_german())
+    
